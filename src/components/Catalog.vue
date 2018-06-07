acb39fb1b01d24039e002b8cb7a91a0667d8eaa8
Ezekiel Mangalu <emangalu@jirnexu.com>
2018-06-07 17:06:20 +0800

    Attempt to test axios implentation on Catalog

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -25,6 +25,7 @@ export default {
     // Get templates
     axios.get('/api/templates')
       .then((response) => { this.templates = response.data })
+      .catch((error) => console.log(error))
   }
 }
 </script>
--- test/unit/specs/Catalog.spec.js
+++ test/unit/specs/Catalog.spec.js
@@ -1,13 +1,23 @@
 import {shallowMount} from '@vue/test-utils'
 import Catalog from '@/components/Catalog'
 
+
+
+jest.mock('axios', () => ({
+  get: jest.fn(() => Promise.resolve({data: [ { title: "Test", slug: "test" } ]}))
+}))
+
+import axios from 'axios'
+
 describe('Catalog.vue', () => {
-  it('should have correct props', () => {
+
+  it('should load templates props', () => {
     const wrapper = shallowMount(Catalog, {
       stubs: ['router-link']
     })
 
-    expect(wrapper.props()).toBeTruthy()
+    expect(axios.get).toBeCalledWith('/api/templates')
+    console.log(wrapper.vm.templates)
   })
 
   it('should match snapshot', () => {
